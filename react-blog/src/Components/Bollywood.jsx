f4cf0dac91bcb847e295e57aeb23cd4a57788f7a
Your Name <your@email.com>
2026-04-24 18:21:21 +0530

    updated

--- react-blog/src/Components/Bollywood.jsx
+++ react-blog/src/Components/Bollywood.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Data from '../Data'
 
 const Bollywood = () => {
+  const Bollywood = Data.filter((item) => item.category === 'Bollywood')
   return (
-    <div>Bollywood</div>
+    <div>{Bollywood.map((bollydata) => (
+      <>
+      <h1>{Bollywood.tittlw}</h1>
+      <img src={bollydata.img_url}/>
+      </>
+    ))}
+    </div>
   )
 }
 
